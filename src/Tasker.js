d198ed17aeee2a9d24f63deeaaffc593f95eaf89
qwertyy _ <moneyalien666@protonmail.com>
2022-10-15 01:49:25 +0200

    [tasker] added validations

--- src/Tasker.js
+++ src/Tasker.js
@@ -1,3 +1,4 @@
+const Validation = require("./Validation");
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 const axios = require('axios');
 
@@ -5,33 +6,37 @@ class Tasker {
     constructor(type, apiKey, websiteURL, websiteKey=undefined, verbose) {
         this.apiKey = apiKey;
         this.verbose = verbose;
-        this.taskData = { "type":type, "websiteURL": websiteURL ? websiteURL.toString() : null, "websiteKey": websiteKey ? websiteKey.toString() : null, };
+        this.parameters = new Validation().parameters;
+        this.taskData = { "type":type, "websiteURL": websiteURL , "websiteKey": websiteKey };
     }
 
     async createTask() {
         let self = this;
         if(this.taskData.websiteURL === null) delete this.taskData.websiteURL;
         if(this.taskData.websiteKey === null) delete this.taskData.websiteKey;
+
+        let v = this.validate(this.taskData);
+
         let axiosConfig = { url: 'https://api.captchaai.io/createTask', headers: { }, method: 'post', data: { "clientKey": this.apiKey.toString(), "task": this.taskData } };
         let r=await axios(axiosConfig)
             .then(async function (response) {
-                if(self.verbose === 2){ console.log(requestData); }
-                if(response.data.errorId !== 0){    // TASK ERROR
-                    return { 'error':-1, 'statusText':response.status+' '+response.statusText, 'apiResponse':response.data }
+                if(self.verbose === 2){ console.log(response.data); }
+                if(response.data.errorId !== 0){    // TASK/SOLVING ERROR
+                    return { 'error':-1, 'statusText':response.status, 'apiResponse':response.data }
                 }
-                return { 'error':0, 'statusText':response.status+' '+response.statusText, 'apiResponse':response.data }
+                return { 'error':0, 'statusText':response.status, 'apiResponse':response.data } // SUCCESS SOLVING
             })
             .catch(function (error) {
                 if(error.response === undefined){ return { 'error':-1, 'statusText':JSON.stringify(error), 'apiResponse':'' } }
                 if(self.verbose === 2){ console.log(error.response.data); }
-                return { 'error':-1, 'statusText':error.response.status+' '+error.response.statusText, 'apiResponse':error.response.data }
+                return { 'error':-1, 'statusText':error.response.status, 'apiResponse':error.response.data }
             });
 
         if(this.verbose === 1 || this.verbose === 2){
             if(r.error === undefined ||  r.error === -1){
-                console.log('[' + this.taskData.type + '][' + axiosConfig.method + '][' + axiosConfig.url + ']['+r.statusText+'][for: '+this.taskData.websiteURL+'][❌][error: '+r.apiResponse.errorDescription+']');
+                console.log('[' + this.taskData.type + ']['+r.statusText+'][' + axiosConfig.url + '][website: '+this.taskData.websiteURL+'][ ❌ ][error: '+r.apiResponse.errorDescription+']');
             }else{
-                console.log('[' + this.taskData.type + '][' + axiosConfig.method + '][' + axiosConfig.url + ']['+r.statusText+'][for: '+this.taskData.websiteURL+'][OK][taskId: '+r.apiResponse.taskId+']');
+                console.log('[' + this.taskData.type + ']['+r.statusText+'][' + axiosConfig.url + '][website: '+this.taskData.websiteURL+'][taskId]['+r.apiResponse.taskId+']');
             }
         }
         return r;
@@ -62,11 +67,36 @@ class Tasker {
                     }
                 });
             if(this.verbose === 1 || this.verbose === 2)
-                console.log('[' + this.taskData.type + '][' + axiosConfig.method + ']['+axiosConfig.url+'][for: ' + (this.taskData.websiteURL ? this.taskData.websiteURL : this.taskData.captchaUrl) + ']['+r.statusText+'][taskId: '+taskId+'][status: '+status+']');
+                console.log('[' + this.taskData.type + ']['+r.statusText+']['+axiosConfig.url+'][for: ' + (this.taskData.websiteURL ? this.taskData.websiteURL : this.taskData.captchaUrl) + '][taskId: '+taskId+'][status: '+status+']');
             if(this.verbose === 2){ console.log(r.apiResponse)}
             if(r.error !== 0) break;
         }
         return r;
+    }
+
+    validate(taskData){
+        if(Object.keys(this.parameters).includes(taskData.type)){ // ?is a existing captcha task
+            this.parameters[taskData.type].forEach(parameter => {
+                if(parameter.required){
+                    if(taskData[parameter.name] === undefined
+                        || String(taskData[parameter.name]).length <= 0
+                        || typeof taskData[parameter.name] !== parameter.type){
+                        throw TypeError(parameter.name+' must by of type '+parameter.type+' and not empty.');
+                    }
+                }else{
+                    if(taskData[parameter.name] !== undefined || null){
+                        if(String(taskData[parameter.name]).length <= 0
+                            || typeof taskData[parameter.name] !== parameter.type) {
+                            throw TypeError(parameter.name+' must by of type '+parameter.type+' and not empty.');
+                        }
+                    }
+                }
+            });
+        }else{
+            throw Error(taskData.type+' is not a valid captcha task type.');
+        }
+        // process.exit(1)
+        return true;
     }
 
 }
---
+++ src/Validation.js
@@ -0,0 +1,105 @@
+class Validation {
+    constructor() { // supported api methods
+        this.parameters = {};
+        this.parameters.HCaptchaTask =
+            [
+                { name:"websiteURL", required: true, type: 'string' }, { name:"websiteKey", required: true, type: 'string' },
+                { name:"proxyType", required: true, type: 'string' }, { name:"proxyAddress", required: true, type: 'string' },
+                { name:"proxyPort", required: true, type: 'number' }, { name:"proxyLogin", required: false, type: 'string' },
+                { name:"proxyPassword", required: false, type: 'string' }, { name:"userAgent", required: false, type: 'string' },
+                { name:"isInvisible", required: false, type: 'boolean' }, { name:"isEnterprise", required: false, type: 'boolean' },
+                { name:"enterprisePayload", required: false, type: 'object' },
+            ];
+        this.parameters.HCaptchaTaskProxyless =
+            [
+                { name:"websiteURL", required: true, type: 'string' }, { name:"websiteKey", required: true, type: 'string' },
+                { name:"userAgent", required: false, type: 'string' }, { name:"isInvisible", required: false, type: 'boolean' },
+                { name:"isEnterprise", required: false, type: 'boolean' }, { name:"enterprisePayload", required: false, type: 'object' },
+            ];
+        this.parameters.HCaptchaClassification = [ { name:"question", required: true, type: 'string' }, { name:"images", required: true, type: 'object' } ];
+        this.parameters.RecaptchaV2Task =
+            [
+                { name:"websiteURL", required: true, type: 'string' }, { name:"websiteKey", required: true, type: 'string' },
+                { name:"proxyType", required: true, type: 'string' }, { name:"proxyAddress", required: true, type: 'string' },
+                { name:"proxyPort", required: true, type: 'number' }, { name:"proxyLogin", required: false, type: 'string' },
+                { name:"proxyPassword", required: false, type: 'string' }, { name:"userAgent", required: false, type: 'string' },
+                { name:"isInvisible", required: false, type: 'boolean' }, { name:"recaptchaDataSValue", required: false, type: 'string' },
+                { name:"cookies", required: false, type: 'string' },
+            ];
+        this.parameters.RecaptchaV2TaskProxyless =
+            [
+                { name:"websiteURL", required: true, type: 'string' }, { name:"websiteKey", required: true, type: 'string' },
+                { name:"userAgent", required: false, type: 'string' }, { name:"isInvisible", required: false, type: 'boolean' },
+                { name:"recaptchaDataSValue", required: false, type: 'string' }, { name:"cookies", required: false, type: 'string' },
+            ];
+        this.parameters.RecaptchaV2EnterpriseTask =
+            [
+                { name:"websiteURL", required: true, type: 'string' }, { name:"websiteKey", required: true, type: 'string' },
+                { name:"proxyType", required: true, type: 'string' }, { name:"proxyAddress", required: true, type: 'string' },
+                { name:"proxyPort", required: true, type: 'number' }, { name:"proxyLogin", required: false, type: 'string' },
+                { name:"proxyPassword", required: false, type: 'string' }, { name:"userAgent", required: false, type: 'string' },
+                { name:"enterprisePayload", required: false, type: 'object' }, { name:"apiDomain", required: false, type: 'string' },
+                { name:"cookies", required: false, type: 'string' },
+            ];
+        this.parameters.RecaptchaV2EnterpriseTaskProxyless =
+            [
+                { name:"websiteURL", required: true, type: 'string' }, { name:"websiteKey", required: true, type: 'string' },
+                { name:"userAgent", required: false, type: 'string' },
+                { name:"enterprisePayload", required: false, type: 'object' }, { name:"apiDomain", required: false, type: 'string' },
+                { name:"cookies", required: false, type: 'string' },
+            ];
+        this.parameters.RecaptchaV3Task =
+            [
+                { name:"websiteURL", required: true, type: 'string' }, { name:"websiteKey", required: true, type: 'string' },
+                { name:"proxyType", required: true, type: 'string' }, { name:"proxyAddress", required: true, type: 'string' },
+                { name:"proxyPort", required: true, type: 'number' }, { name:"proxyLogin", required: false, type: 'string' },
+                { name:"proxyPassword", required: false, type: 'string' }, { name:"pageAction", required: true, type: 'string' },
+                { name:"minScore", required: false, type: 'number' },
+            ];
+        this.parameters.RecaptchaV3TaskProxyless =
+            [
+                { name:"websiteURL", required: true, type: 'string' }, { name:"websiteKey", required: true, type: 'string' },
+                { name:"pageAction", required: true, type: 'string' }, { name:"minScore", required: false, type: 'number' },
+            ];
+        this.parameters.ReCaptchaV2Classification = [ { name:"question", required: true, type: 'string' }, { name:"image", required: true, type: 'string' } ];
+        this.parameters.GeeTestTask =
+            [
+                { name:"websiteURL", required: true, type: 'string' }, { name:"gt", required: true, type: 'string' },
+                { name:"challenge", required: true, type: 'string' }, { name:"geetestApiServerSubdomain", required: true, type: 'string' },
+                { name:"proxyType", required: true, type: 'string' }, { name:"proxyAddress", required: true, type: 'string' },
+                { name:"proxyPort", required: true, type: 'number' }, { name:"proxyLogin", required: false, type: 'string' },
+                { name:"proxyPassword", required: false, type: 'string' }, { name:"userAgent", required: false, type: 'string' },
+                { name:"geetestGetLib", required: false, type: 'string' }, { name:"version", required: false, type: 'number' },
+                { name:"initParameters", required: false, type: 'object' },
+            ];
+        this.parameters.GeeTestTaskProxyless =
+            [
+                { name:"websiteURL", required: true, type: 'string' }, { name:"gt", required: true, type: 'string' },
+                { name:"challenge", required: true, type: 'string' }, { name:"geetestApiServerSubdomain", required: true, type: 'string' },
+                { name:"userAgent", required: false, type: 'string' }, { name:"geetestGetLib", required: false, type: 'string' },
+                { name:"version", required: false, type: 'number' }, { name:"initParameters", required: false, type: 'object' },
+            ];
+        this.parameters.DataDomeSliderTask =
+            [
+                { name:"websiteURL", required: true, type: 'string' }, { name:"captchaUrl", required: true, type: 'string' },
+                { name:"userAgent", required: true, type: 'string' }, { name:"proxyType", required: true, type: 'string' },
+                { name:"proxyAddress", required: true, type: 'string' }, { name:"proxyPort", required: true, type: 'number' },
+                { name:"proxyLogin", required: false, type: 'string' }, { name:"proxyPassword", required: false, type: 'string' },
+            ];
+        this.parameters.FunCaptchaTask =
+            [
+                { name:"websiteURL", required: false, type: 'string' }, { name:"websitePublicKey", required: true, type: 'string' },
+                { name:"funcaptchaApiJSSubdomain", required: true, type: 'string' }, { name:"data", required: false, type: 'string' },
+                { name:"userAgent", required: false, type: 'string' }, { name:"proxyType", required: true, type: 'string' },
+                { name:"proxyAddress", required: true, type: 'string' }, { name:"proxyPort", required: true, type: 'number' },
+                { name:"proxyLogin", required: false, type: 'string' }, { name:"proxyPassword", required: false, type: 'string' },
+            ];
+        this.parameters.FunCaptchaTaskProxyless =
+            [
+                { name:"websiteURL", required: false, type: 'string' }, { name:"websitePublicKey", required: true, type: 'string' },
+                { name:"funcaptchaApiJSSubdomain", required: true, type: 'string' }, { name:"data", required: false, type: 'string' },
+                { name:"userAgent", required: false, type: 'string' },
+            ];
+    }
+}
+module.exports = Validation
