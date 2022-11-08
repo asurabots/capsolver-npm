2084ef83f3676783eafc4b123ac8189834e060d3
qwertyy _ <moneyalien666@protonmail.com>
2022-11-08 23:05:08 +0100

    [tasker] refactoring

--- src/Tasker.js
+++ src/Tasker.js
@@ -16,19 +16,19 @@ class Tasker {
     async createTask(url=undefined) {
         let self = this;
         this.validate(this.taskData);
-        let axiosConfig = { url: (url === undefined) ? 'https://api.captchaai.io/createTask' : url, headers: { }, method: 'post', data: { 'clientKey': this.apikey.toString(), 'appId': 'AF0F28E5-8245-49FD-A3FD-43D576C0E9B3', 'task': this.taskData } };
-        let handled = await axios(axiosConfig)
+        let req = { url: (url === undefined) ? 'https://api.captchaai.io/createTask' : url, headers: { }, method: 'post', data: { 'clientKey': this.apikey.toString(), 'appId': 'AF0F28E5-8245-49FD-A3FD-43D576C0E9B3', 'task': this.taskData } };
+        let handled = await axios(req)
             .then(async function (response) {
                 if(self.verbose === 2){ console.log(response.data); }
                 if(response.data.errorId !== 0){ return { 'error':-1, 'statusText':response.status, 'apiResponse':response.data } }
-                return { 'error':0, 'statusText':response.status, 'apiResponse':response.data }
+                return { 'error':0, 'statusText':response.status, 'apiResponse':response.data, 'solution':response.data.solution }
             })
             .catch(function (error) {
                 if(error.response === undefined){ return error; }
                 if(self.verbose === 2){ console.log(error.response.data); }
                 return { 'error':-1, 'statusText':error.response.status, 'apiResponse':error.response.data }
             });
-        if(this.verbose !== 0){ console.log('[' + this.taskData.type + ']['+r.statusText+'][' + axiosConfig.url + ']'); }
+        if(this.verbose !== 0){ console.log('[' + this.taskData.type + ']['+handled.statusText+'][' + req.url + ']'); }
         return handled;
     }
 
@@ -41,11 +41,11 @@ class Tasker {
         let self = this; let status = ''; let fails = 0; let handled = null;
         if(taskId === undefined) return;
         let requestData = { 'clientKey':this.apikey, 'taskId': taskId };
-        let axiosConfig = { method: 'post', url: 'https://api.captchaai.io/getTaskResult', headers: { }, data: requestData };
+        let req = { method: 'post', url: 'https://api.captchaai.io/getTaskResult', headers: { }, data: requestData };
         while(status !== 'ready'){
             await sleep(rqDelay);
             if(fails > 10) break;
-            handled = await axios(axiosConfig)
+            handled = await axios(req)
                 .then(async function (response) {
                     if(self.verbose === 2){ console.log(requestData); }
                     if(response.data.errorId !== 0){
@@ -53,7 +53,7 @@ class Tasker {
                         return { 'error':-1, 'statusText':response.status, 'apiResponse':response.data }
                     }
                     status = response.data.status;
-                    return { 'error':0, 'statusText':response.status, 'apiResponse':response.data }
+                    return { 'error':0, 'statusText':response.status, 'apiResponse':response.data, 'solution':response.data.solution }
                 })
                 .catch(function (error) {
                     if(self.verbose === 2){ console.log(error.response.data); }
@@ -61,9 +61,9 @@ class Tasker {
                     return { 'error':-1, 'statusText':error.response.status, 'apiResponse':error.response.data }
                 });
             if(this.verbose !== 0)
-                console.log('[' + this.taskData.type + ']['+r.statusText+'][api.captchaai.io/getTaskResult][for: ' + (this.taskData.websiteURL ? this.taskData.websiteURL : this.taskData.captchaUrl) + '][taskId: '+taskId+'][status: '+status+']');
-            if(this.verbose === 2){ console.log(r.apiResponse)}
-            if(r.error !== 0) break;
+                console.log('[' + this.taskData.type + ']['+handled.statusText+'][api.captchaai.io/getTaskResult][for: ' + (this.taskData.websiteURL ? this.taskData.websiteURL : this.taskData.captchaUrl) + '][taskId: '+taskId+'][status: '+status+']');
+            if(this.verbose === 2){ console.log(handled.apiResponse)}
+            if(handled.error !== 0) break;
         }
         return handled;
     }
