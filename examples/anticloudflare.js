132780e2c57316360ce338e0809cb2e4b0442d8c
0qwertyy <xamako2003@protonmail.com>
2023-03-02 22:21:11 +0100

    [examples]

--- examples/anticloudflare.js
+++ examples/anticloudflare.js
@@ -2,9 +2,8 @@ const CapSolver = require('../src/CapSolver');
 const handler = new CapSolver('CAI-XXXX ...');
 
 (async function() {
-    await handler.antiturnstile(
-        'https://website.com/',
-        'sitekey',
+    await handler.anticloudflare(
+        'https://minecraftpocket-servers.com/login/',
         {'proxy': 'proxy.provider.io:23331:user1:password1'},
         {'type':'challenge'}
     ).then((captcha) => { if(captcha.error !== 0){ console.log(captcha.apiResponse) }else{ console.log(captcha.solution) } })
--- examples/pm2/ecosystem.config.js
+++ examples/pm2/ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     apps: [
-        { name: "1", script: "../recaptchav3.js" },
-        { name: "2", script: "../recaptchav3.js" },
-        { name: "3", script: "../recaptchav3.js" },
-        { name: "4", script: "../recaptchav3.js" },
-        { name: "5", script: "../recaptchav3.js" },
-        { name: "6", script: "../recaptchav3.js" },
-        { name: "7", script: "../recaptchav3.js" },
-        { name: "8", script: "../recaptchav3.js" },
-        { name: "9", script: "../recaptchav3.js" },
+        { name: "1", script: "../antiturnstile.js" },
+        { name: "2", script: "../antiturnstile.js" },
+        { name: "3", script: "../antiturnstile.js" },
+        { name: "4", script: "../antiturnstile.js" },
+        { name: "5", script: "../antiturnstile.js" },
+        { name: "6", script: "../antiturnstile.js" },
+        { name: "7", script: "../antiturnstile.js" },
+        { name: "8", script: "../antiturnstile.js" },
+        { name: "9", script: "../antiturnstile.js" },
     ]
 }
